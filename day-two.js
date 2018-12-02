bc2266c0e55a72c95f30fb172636a419fede12bc
Paul Vallis <vallis1@gmail.com>
2018-12-02 22:07:07 +0000

    Day two

---
+++ day-two.js
@@ -0,0 +1,50 @@
+const inputs = ['pnebjqralgdgckzfifvtxywomu', 'pnebjqsalrdgcqzfihotxhwomu', 'pneajqsalrdgckzfihytxywoml', 'pnepjqsalrwgckztihvtxywomu', 'pnhbjqsalrdgckzfimvtxywodu', 'pnwbjqsdlrdgckzfihvnxywomu', 'inebjqnalrdgckzfihvtxzwomu', 'pnebjssalhdgckzfihvtsywomu', 'pnebjqjalrdgckzfiavtxywoku', 'vnebjqsalrdgckzfihvbxmwomu', 'phebjksaurdgckzfihvtxywomu', 'pneojqealrdgckzhihvtxywomu', 'snebjqsalrdgckzqihvtxyzomu', 'pnebjqsalrtackzfihvtxswomu', 'bnebjqlalrdgckzfihvtxywhmu', 'pnebjqfalrdgckzfijvtxywomi', 'fnehjbsalrdgckzfihvtxywomu', 'pnebjasalrdgckzdihvtxqwomu', 'pnebjhsaljdgckzfihvtxywmmu', 'pnebjqsalrdgckzfihvsxykoau', 'pnebjqsalrdgckzbihvtdywomc', 'pnobjqsalrxgckzfihvtxywomh', 'pnebjqstlrdgchzfihvtxywnmu', 'pnebjquaxrdgckzfihvtxywolu', 'pqebjqsalrdgcdzfihvtcywomu', 'xnabjqsalrdgckzfihvtxywmmu', 'rnebjqsalrdgckzfihvtxmwouu', 'vaebjqsalrdgckcfihvtxywomu', 'pnebjqsalrpgcnzfihvbxywomu', 'pcvbjqsalrdjckzfihvtxywomu', 'pneyjqsafrdgckzfihdtxywomu', 'pxedjqsalrdgckzfihvtxyzomu', 'pnebjqsalrdgctzfihnyxywomu', 'pnebjqsalrdgckzfihvtnylsmu', 'pnebjqsalrdyckzfihvbxycomu', 'fnebjqsalrdgckzfihvtxtwomc', 'pnobjqsalrdgckdfihvtxywomh', 'pqebjqsalrdgcqzfihvtxywymu', 'pnebxqsalrdgckzficvtwywomu', 'pnebjqshlragczzfihvtxywomu', 'pnebqqsalrdackzfihttxywomu', 'pnebjqsalrdsckwfbhvtxywomu', 'pnehjqsalrdgcuzfxhvtxywomu', 'pnebjqsavrdgckzfihvexywomn', 'pnebunsalrdgckzfihvtxywomi', 'pnebjxsalrdgckzfmhvtpywomu', 'rnebjqsalrdghkzfihztxywomu', 'pnebjqsalrigcbzfihvfxywomu', 'pnebqqsalrggckzfihvtxyromu', 'pnebjqsalrdgchzfihvtxylmmu', 'pnebeqsalrdgckzdihvtxywoms', 'pnebjqsalrdgckzzihvfxywozu', 'pnzbjgsalrtgckzfihvtxywomu', 'pnebjqsaledgckzjihvtxzwomu', 'pnebjqsalydgckqfihvtxywouu', 'pnebjqsalrdgckufihvqxdwomu', 'pnebjqsylrdgckzfihvdxyjomu', 'pnemjqsalrdgckzeihvtxywoqu', 'plebjasalrdgckzfihvtxywomb', 'pnebjqsadrdgckufihvtxyfomu', 'pbebjqsaardgckzfihvtxmwomu', 'pnebjqsalrdgcmzfihotxywgmu', 'pnebjqsaprdgcizfihvtxywhmu', 'pnebjqsalrkgcuzfihvtlywomu', 'pnebjqsalrdnckzfihvtxysomg', 'pnebjqdafrdgckzfihctxywomu', 'pnebjqsalrdgckzfihutxkwomp', 'pnebvqsalrdgclzfimvtxywomu', 'pnebjqralrdgcktfihvtxiwomu', 'pneujqsalrdsckzfzhvtxywomu', 'pnebfqgalrdgckzfihvtxywjmu', 'pneyjqsalrkgckzfihctxywomu', 'pndbjqsalrdgckzfjhvtxywouu', 'pneljnsalrdgcozfihvtxywomu', 'phebjqsalrdgckzfihxtxdwomu', 'pnlbjqsalrhgckzfzhvtxywomu', 'pnebjqsalrsgckzfiovtxywwmu', 'pncbjqsalrdgfkzfivvtxywomu', 'nnebjqsalrdgckzfthvtxycomu', 'pnebjqwalsdgckzfixvtxywomu', 'pnebjtsalrdgcfzfimvtxywomu', 'pnebjqsvlrdgckzfihutxfwomu', 'pnebjmsalrdgckzkxhvtxywomu', 'pnekjqsllrdgckzfinvtxywomu', 'pneijqsxlrdgckzfihvtxywjmu', 'wnxbjqsafrdgckzfihvtxywomu', 'pnebjqskledgokzfihvtxywomu', 'pnebjqvalrdgckzfihvtxytoju', 'pneqjqsalrdgckzfilvthywomu', 'pnebjqsalrdgckzfihvokywomf', 'bnebjqsalrdgckufihvtxywimu', 'pnebjqsaurdgckzfihvtrywosu', 'pnebjmsaludgckzfihvtxywomn', 'pnebdqsalrdgcktfihvtxywodu', 'pnebjqjylzdgckzfihvtxywomu', 'piebjqsalrdgcrzfihstxywomu', 'pnebjqsaurdgckwfnhvtxywomu', 'pnebxqsajrdgcjzfihvtxywomu', 'pnebjqsalrdghsdfihvtxywomu', 'pnebcqsxlrdgckzfihvtxyaomu', 'pnefjqsalrdgckzfuhvtxyworu', 'pnebjqsalrdlcksfihvteywomu', 'pnebjqlalrgackzfihvtxywomu', 'pnebdqsalrdickzfihvtxdwomu', 'pneujksalrdgctzfihvtxywomu', 'pnebjqsalrduckzfihvsxywomf', 'pnebjqsalrdgckcfihotxywomd', 'envbjqsalsdgckzfihvtxywomu', 'pnebjqsalzdgcvzzihvtxywomu', 'pnebjqsalrdyckzflhvyxywomu', 'pnebjqsalrdglkzfihstxymomu', 'pnebmqsalrdgokzfihvtxywoml', 'pnebjqsylrdnckzfihatxywomu', 'pnebjqaflndgckzfihvtxywomu', 'pneboqsagragckzfihvtxywomu', 'peebjqstlndgckzfihvtxywomu', 'onebjqsklrdgckzfihvtxmwomu', 'pnebjqjnlrdgckrfihvtxywomu', 'pnebjqsalrhgckzfihvqxywomh', 'pnebjqsalrdgckzzihvtxowomw', 'pnebjgsalrdgckffihltxywomu', 'znebaqsalcdgckzfihvtxywomu', 'pnnbjqeasrdgckzfihvtxywomu', 'rnebjqaalrxgckzfihvtxywomu', 'pnebjqsalrdgckaxphvtxywomu', 'pnebjcnalrdgnkzfihvtxywomu', 'pnebjasalbdgckzmihvtxywomu', 'pnebjqsalrdgckefjhvtmywomu', 'pnebjqsalrdgmkzfihvtxyoomb', 'pnebjqsalrkgckogihvtxywomu', 'pnwbjqsalrdgckztihvtxywomt', 'pnebjqsalrdgckzfihotgnwomu', 'pnebjqsdlrrgckzfihvtxyaomu', 'pnebvasalrdgckzfihvtsywomu', 'pnebrqqalrvgckzfihvtxywomu', 'tnebjqsglrdgqkzfihvtxywomu', 'pnebjqsatrsgckifihvtxywomu', 'pneboqsalrdgckzfihvkxywomi', 'pnezaqsalrdgcktfihvtxywomu', 'pnebjqsnlrdgckzfihvfxqwomu', 'pneajqsaxrmgckzfihvtxywomu', 'pnebjosalodgckzfihvxxywomu', 'pnebjqsalndgckmfihvtfywomu', 'pneejqsalidgckzfihgtxywomu', 'pnecjqsalrdgckzfihptxiwomu', 'tnebjqsalrdgckznihvxxywomu', 'ptebjqsalrdgckzfimvtxywomm', 'wnebjqsalndgckzfihvtxywoju', 'fnebmqsplrdgckzfihvtxywomu', 'pnlbjqsalrdghkzficvtxywomu', 'pnebjqsesrdgckzdihvtxywomu', 'pnebjqsalregokzfirvtxywomu', 'pnebjtualrtgckzfihvtxywomu', 'pnebjwsdlrdgckzfihvtxywoml', 'pnlbjqsayrdgckzfqhvtxywomu', 'pnebjwsalpdgckzfihvtxywomc', 'pnqbjqsalcdgckzhihvtxywomu', 'pneujqsalrdgckzfhhvtxrwomu', 'pnebjqsalqdgcizfihvtxywimu', 'pnebjqsacldgckzfihvwxywomu', 'puebjqsalrdgckzfbhvtxyeomu', 'pnebjqsalrdgcyimihvtxywomu', 'pnebjlsalrdgckzfihvtxiwome', 'pnebfusalrdgckzfihvtxywodu', 'pnebjqsalrdgvazfirvtxywomu', 'pnebjqsalrdgckyfohvtxywomz', 'gnenjqsalrdgckzfihvtxynomu', 'mnebjqsalrdgckhfihvtxycomu', 'phebjqsalrdgckzfihvtxtworu', 'pnebjqsalrdgdkzfihvtxywfmj', 'pneveqsairdgckzfihvtxywomu', 'pnebjqsalcdlckzfihvtxywomg', 'pneajqsalrdgckzfihvtxygoxu', 'puebjqdclrdgckzfihvtxywomu', 'tuebjqsalrdgckzfihvtxywoou', 'pwenjqsalrdgckzfihvtxywomg', 'pnebjqsalrdgckzfihhltywomu', 'pnebjqsalrdgchzqievtxywomu', 'pnegjqsalrdgckzfiovtxywdmu', 'pnebjaralrqgckzfihvtxywomu', 'pnebjqsalrdrckzfimvtxywomm', 'pnebjqsalrdgckzfpgvtxewomu', 'pnebjqsalrdhcqzfihitxywomu', 'pnebjqsalrjgckefihmtxywomu', 'pnebjcsalrdgcksfikvtxywomu', 'pnebjqsalrdgckzfihvtxywdjc', 'pnebjqsazrjgckzjihvtxywomu', 'pnfbjqsclrdgckzfihvtxybomu', 'pnebjqsalrdgckuqihvtxyaomu', 'pfpbjzsalrdgckzfihvtxywomu', 'pnevjqsalrdgckwfihytxywomu', 'pnebjqsqlrkgckzfihvtvywomu', 'pneejqsalrdlckzfihvtxywopu', 'pnebjqsalcdgxkzfihvtxywomd', 'pneqjqsalrdgcvzzihvtxywomu', 'pnvbjqsalydgctzfihvtxywomu', 'pnebjqsalrdgckzzihvfxywomn', 'pnybjqsaerdgckzfihstxywomu', 'pnobjqsalrdkckzfihvtxywomv', 'pnebjqsalridckzfihvtxywfmu', 'pnhbjqsaludgckyfihvtxywomu', 'pnetjqsaprdgykzfihvtxywomu', 'wnebjqsalrdvcfzfihvtxywomu', 'pnetjqsalrdmckwfihvtxywomu', 'pnebjysalrdgcszfihvtxnwomu', 'pnebjqsrlrdgckzfihvtxywkhu', 'pnubjqsplrdgcjzfihvtxywomu', 'pnebjqsalrdzckzficjtxywomu', 'pnebjqsalregckzfinvtxywoku', 'pnebjqsalrcgckyfivvtxywomu', 'pyenjqsalrdgckzfihvnxywomu', 'prebjqsalrdnckzfihvtxysomg', 'pnebjnsalrdgchzfihvaxywomu', 'pnebjqsalrdgckzfihxagywomu', 'pnebjqsalrdgckzvihvtoywoml', 'pnebjqsilrdgckzfihvtfywgmu', 'pnebjqmalrdgckzfihvtvawomu', 'pnebqqsalrdgckzfiuvtfywomu', 'pneqjqsalrdgckzfihvqxywomi', 'pnebjesalrsgckzfihvtxywmmu', 'znebjqsblrdgckzfihvlxywomu', 'pnebjqsalrdgckzfuhvtlyworu', 'pnebjqsylrdgckzfihvqxpwomu', 'onebjqsalfdgckifihvtxywomu', 'pnebjusalrdgckzfihvtxywyml', 'pnebjssflrdgckzfigvtxywomu', 'pnebjfsdzrdgckzfihvtxywomu', 'pnebjqsalrdgcktfihvixywocu', 'gnebjqnaqrdgckzfihvtxywomu', 'pnebjqsaqrugckzfihhtxywomu', 'pnebjqsxlrdgckzfihvtxlwosu', 'pnebjzsalrdgckzmihvtxywovu', 'pnebgqsalrdgckzfizvtxyjomu', 'pnebjqsmlrdgckzfihvtxywsmi', 'pnebjqsakmdgckzjihvtxywomu', 'pnebjqdglrdgckvfihvtxywomu', 'pnebmhsalrdgckxfihvtxywomu', 'pneejqsalrdlckzfihvnxywomu', 'bnebjqsalmdgckzfihvfxywomu', 'bnebjnsalrdgcizfihvtxywomu', 'pnebjqsalhdgcdzfihvbxywomu', 'pnebjqsjlrdgckzfihvgiywomu', 'pnebjisalrdgckzfihvtxywqmi', 'pdebjqsalrdickzfihhtxywomu', 'pnebjqsalrdkckzfihvjeywomu', 'pneyjqsalrqgckzfihvtxywohu', 'pnebjqsalrdgckcfihvtxjlomu', 'plebqwsalrdgckzfihvtxywomu', 'pnebjqlalrdgckzfihetxynomu', 'sngbjqsalrdgckzfihvmxywomu']
+
+const partOne = () => {
+  // const inputs = ['abcdef', 'bababc', 'abbcde', 'abcccd', 'aabcdd', 'abcdee', 'ababab']
+  
+  const countInstances = (char, _, charArray) => ({ [char]: charArray.filter(x => x === char).length })
+  const toCountsDictionary = ([dict], curr) => [{ ...dict, ...curr }]
+
+  const occurrences = value => value.split('')
+    .map(countInstances)
+    .reduce(toCountsDictionary, [{}])
+    .map(dict => dict)
+
+  const run = data => data.reduce((p, c) => {
+    const [dict] = occurrences(c)
+    const two = Object.values(dict).some(v => v === 2);
+    const three = Object.values(dict).some(v => v === 3);
+
+    return [
+      two ? p[0] + 1 : p[0],
+      three ? p[1] + 1 : p[1],
+    ]
+  }, [0, 0])
+
+  const [twos, threes] = run(inputs)
+  const result = twos * threes
+
+  console.log(result)
+}
+
+const partTwo = () => {
+  const test = ['abcde', 'fghij', 'klmno', 'pqrst', 'fguij', 'axcye', 'wvxyz'] 
+
+  const matchingChars = (s1, s2) => {
+    const s1a = s1.split('')
+    const s2a = s2.split('')
+
+    return s1a.map((c, i) => c === s2a[i])
+  }
+  const toMatchMaps = (item, all) => all.map(x => matchingChars(item, x))
+  const matches = data => data.reduce((p, c, _, a) => ({ ...p, [c]: toMatchMaps(c, a) }), {})
+
+  const findOffByOne = obj => Object.keys(obj)
+    .filter(key => obj[key].map(boolArray => boolArray.filter(b => b === false).length === 1).find(x => x === true))
+  
+  console.log(findOffByOne(matches(inputs)))
+}
+
+// partOne()
+partTwo()
